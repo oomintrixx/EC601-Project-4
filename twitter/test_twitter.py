2d665bb5014a3493842ef89e5199a559efd9f22f
Shashank <shashankmanjunath14@gmail.com>
2020-10-31 14:24:42 -0400

    Finished Twitter API Tests

--- twitter/test_twitter.py
+++ twitter/test_twitter.py
@@ -1,14 +1,11 @@
 from twitter.tweets import Tweets
+import tweepy
 import pytest
+import os
 
 
 class TestTwitterAPIBase:
-    VAR = 3
-    DATA = 4
-
-    def test_var_positive(self):
-        assert self.VAR >= 0
-
+    t = Tweets(os.environ.get("TWITTER_API_CONFIG"))
 
 class TestConfigLoading:
     """ Tests config file loading  """
@@ -20,7 +17,7 @@ class TestConfigLoading:
     def test_empty_key_config(self):
         config_file = "tests/config_empty_2.ini"
         with pytest.raises(RuntimeError):
-            self.assertTrue(Tweets(config_file))
+            Tweets(config_file)
 
     def test_default_config(self):
         config_file = "config.ini"
@@ -30,20 +27,49 @@ class TestConfigLoading:
 
 class TestTweetGrab(TestTwitterAPIBase):
     def test_sds(self):
-        pass
+        num_sds_tweets = 100
+        sds_tweets = self.t.grab_tweets("SDS", num_tweets=num_sds_tweets)
+        assert len(sds_tweets) == num_sds_tweets
 
     def test_fake_twitter(self):
-        pass
+        """ Testing pulling tweets from an account that does not exist """
+        fake_twitter_name = "ijljkl;j"
+        with pytest.raises(tweepy.error.TweepError):
+            self.t.grab_tweets(fake_twitter_name, num_tweets=10)
+
+    def test_no_tweets(self):
+        """ Testing pulling tweets from an account that has never tweeted """
+        empty_twitter_name = "ShashankManjun5"
+        tw = self.t.grab_tweets(empty_twitter_name, num_tweets=10)
+        assert len(tw) == 0
 
     def test_num_tweets_100(self):
         """ Tests pulling default number of tweets """
-        pass
+        default_num_tweets = 100
+        sds_tweets = self.t.grab_tweets("SDS")
+        assert len(sds_tweets) == default_num_tweets
 
     def test_num_tweets_1(self):
         """ Test pulling a single tweet """
-        pass
+        num_tweets = 1
+        sds_tweets = self.t.grab_tweets("SDS", num_tweets=num_tweets)
+        assert len(sds_tweets) == num_tweets
 
     def test_num_tweets_0(self):
         """ Test pulling no tweets """
-        pass
+        num_tweets = 0
+        sds_tweets = self.t.grab_tweets("SDS", num_tweets=num_tweets)
+        assert len(sds_tweets) == num_tweets
+
+    def test_num_tweets_36(self):
+        """ Test pulling less than initial pull of tweets """
+        num_tweets = 36
+        sds_tweets = self.t.grab_tweets("SDS", num_tweets=num_tweets)
+        assert len(sds_tweets) == num_tweets
+
+    def test_num_tweets_112(self):
+        """ Test pulling more than double initial pull of tweets """
+        num_tweets = 112
+        sds_tweets = self.t.grab_tweets("SDS", num_tweets=num_tweets)
+        assert len(sds_tweets) == num_tweets
 
